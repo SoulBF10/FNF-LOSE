e275d32c4bc4cc511c5f32f6ce75cfbca393ab5c
glauberb0802 <111463416+glauberb0802@users.noreply.github.com>
2023-09-12 16:07:07 -0300

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1142,11 +1142,6 @@ class PlayState extends MusicBeatState
 		botplayTxt.borderSize = 1.25;
 		botplayTxt.visible = cpuControlled;
 		add(botplayTxt);
-var creditTxt = new FlxText(876, 648, 348);
-     creditTxt.text = "PORT BY\nglauber01";
-    creditTxt.setFormat(Paths.font("vcr.ttf"), 30, FlxColor.WHITE, RIGHT, FlxTextBorderStyle.OUTLINE,FlxColor.BLACK);
-    creditTxt.scrollFactor.set();
-    add(creditTxt);
 		if(ClientPrefs.downScroll) {
 			botplayTxt.y = timeBarBG.y - 78;
 		}
